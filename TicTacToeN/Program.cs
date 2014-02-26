40bc49a97d7282b215ea12a03fabc367376f1bc4
VegaFromLyra <asha.ab@gmail.com>
2014-02-25 18:09:21 -0800

    Tic Tac Toe for board of size n

---
+++ TicTacToeN/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe game = new TicTacToe(4);
+
+            Piece[,] input1 = new Piece[,] {
+                                             {Piece.X, Piece.X, Piece.X, Piece.X},
+                                             {Piece.O, Piece.X, Piece.O, Piece.O},
+                                             {Piece.X, Piece.O, Piece.O, Piece.X},
+                                             {Piece.X, Piece.O, Piece.O, Piece.X},
+                                          };
+            game.Init(input1);
+            Console.WriteLine("Test case 1: Winner is {0}", game.HasWon());
+
+
+            Piece[,] input2 = new Piece[,] {
+                                             {Piece.O, Piece.X, Piece.X, Piece.X},
+                                             {Piece.O, Piece.X, Piece.O, Piece.X},
+                                             {Piece.O, Piece.O, Piece.X, Piece.X},
+                                             {Piece.O, Piece.O, Piece.X, Piece.O}
+                                          };
+            game.Init(input2);
+            Console.WriteLine("Test case 2: Winner is {0}", game.HasWon());
+
+
+            Piece[,] input3 = new Piece[,] {
+                                             {Piece.X, Piece.O, Piece.X, Piece.O},
+                                             {Piece.O, Piece.X, Piece.O, Piece.X},
+                                             {Piece.O, Piece.X, Piece.X, Piece.O},
+                                             {Piece.O, Piece.O, Piece.X, Piece.X}
+                                          };
+            game.Init(input3);
+            Console.WriteLine("Test case 3: Winner is {0}", game.HasWon());
+
+            Piece[,] input4 = new Piece[,] {
+                                             {Piece.X, Piece.O, Piece.O, Piece.O},
+                                             {Piece.X, Piece.O, Piece.O, Piece.X},
+                                             {Piece.O, Piece.O, Piece.X, Piece.X},
+                                             {Piece.O, Piece.X, Piece.X, Piece.X}
+                                          };
+            game.Init(input4);
+            Console.WriteLine("Test case 4: Winner is {0}", game.HasWon());
+
+        }
+    }
+}
---
+++ TicTacToeN/TicTacToe.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeN
+{
+    enum Piece
+    {
+        Empty,
+        O,
+        X
+    };
+
+    class TicTacToe
+    {
+        private int N;
+
+        private Piece[,] board;
+   
+        public TicTacToe(int N)
+        {
+           this.N = N;
+        }
+ 
+        public void Init(Piece[,] board)
+        {
+           this.board = board;
+        }
+
+        public Piece HasWon()
+        {
+            int row = 0;
+            int col = 0;
+
+            // Chek rows
+            for(row = 0; row < N; row++)
+            {
+                 if (board[row,0] != Piece.Empty)
+                 {
+                     for (col = 0; col < N - 1; col++)
+                     {
+                         if (board[row,col] != board[row, col+1])
+                         {
+                             break;
+                         }
+                     }
+
+                     if (col == N - 1)
+                     {
+                         return board[row,0];
+                     }
+                 }
+            }
+
+            // Check columns
+            for(col = 0; col < N; col++)
+            {
+                 if (board[0, col] != Piece.Empty)
+                 {
+                     for(row = 0; row < N - 1; row++)
+                     {
+                         if (board[row, col] != board[row + 1, col])
+                         {
+                            break;
+                         }
+                     }
+
+                     if (row == N - 1)
+                     {
+                         return board[0, col];
+                     }        
+                 }
+            }
+
+            // Check diagonal 1 (top left to bottom right)
+            if (board[0,0] != Piece.Empty)
+            {
+               for(row = 0; row < N - 1; row++)
+               {
+                   if (board[row, row] != board[row + 1, row + 1])
+                   {
+                       break;
+                   }
+               }
+
+               if (row == N - 1)
+               {
+                   return board[0,0];
+               }          
+            }
+
+            // Check diagonal 2 (bottom left to top right)
+            if (board[N - 1, 0] != Piece.Empty)
+            {
+               for(row = N - 1, col = 0; row >=1 && col < N - 1; row--, col++)
+               {
+                   if (board[row, col] != board[row - 1, col + 1])
+                   {
+                       break;
+                   }               
+               }
+
+               if (row == 0 && col == N - 1)
+               {
+                   return board[N - 1,0];
+               }    
+            }         
+
+            return Piece.Empty;
+        } 
+    }
+}
